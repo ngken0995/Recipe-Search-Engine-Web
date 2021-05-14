8502620627bd3ede5c49e4d1c2d89795b01e1e09
unknown <kng66@gatech.edu>
2021-05-13 22:05:11 -0400

    create home view

--- nutrition_web/urls.py
+++ nutrition_web/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from feeds.views import home_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home_view),
+    path('feeds/<int:feed_id>', home_view)
 ]
