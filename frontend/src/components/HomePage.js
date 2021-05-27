ea225c6ba6df751bcc9a67cc200c609aeffdc5ee
unknown <kng66@gatech.edu>
2021-05-27 08:59:07 -0400

    Spoonacular connect to React

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -7,20 +7,44 @@ import {
   Redirect,
 } from "react-router-dom";
 
+import { Grid, Button, Typography } from "@material-ui/core";
+
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      foods:[],
+    }
+
+    this.getInitiatedFood = this.getInitiatedFood.bind(this);
+  }
+
+  componentDidMount() {
+    this.getInitiatedFood();
+  }
+
+  getInitiatedFood() {
+    fetch("/spoonacular/")
+      .then((response) => {
+        if (!response.ok) {
+          return {};
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        this.setState({ foods: data });
+        console.log(this.state.foods);
+      });
   }
 
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <p>This is the home page</p>
-          </Route>
-        </Switch>
-      </Router>
+      <Grid>
+          <div>{this.state.foods.map((food) => (
+            <img src={food.image} alt='na'/>
+            ))}</div>
+      </Grid>
     );
   }
 }
--- nutrition_web/settings.py
+++ nutrition_web/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'frontend.apps.FrontendConfig'
+    'frontend.apps.FrontendConfig',
+    'spoonacular.apps.SpoonacularConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
