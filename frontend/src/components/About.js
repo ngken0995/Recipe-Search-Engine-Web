2ce6888131be88cb4fe7e975fd98d0e7f105b91f
unknown <kng66@gatech.edu>
2021-05-28 15:47:33 -0400

    add recipe to About.js

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -3,45 +3,6 @@ import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
 import axios from 'axios'
 
-/*
-export default function About() {
-
-    const { id } = useParams();
-    const [steps, setSteps] = useState(null);
-
-    useEffect(() => {
-        getSteps();
-    }, [])
-
-    const getSteps = () =>{
-        axios({
-            method: 'GET',
-            url: `https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=b19b9ab34ae04c10aeefb0f9ef7e0f3b`,    
-        }).then(res => {
-            const recipeSteps = res.data[0].steps;
-            setSteps(recipeSteps);
-            console.log(steps)
-        })
-        .catch(error => {
-            dispatch({ type: AUTH_FAILED });
-            dispatch({ type: ERROR, payload: error.data.error.message });
-        });
-        return (
-            <FoodSteps steps={steps}/>
-        )
-    }
-
-    /*
-    return (
-        <div>
-            {id}
-            {steps.map((step) => {
-                return <div key={step.number}>{step.step}</div>
-            })}
-        </div>
-    )
-}
-*/
 export default function About() {
     const [steps, setSteps] = useState([])
     const [image, setImage] = useState('')
